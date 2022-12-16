24e1136f4509f435b09064a5c24fa7f42004bf8e
{ ow3n } <35806309+wtfow3n@users.noreply.github.com>
2022-12-15 21:38:58 -0500

    Create ExesAndOhs.py

---
+++ ExesAndOhs.py
@@ -0,0 +1,15 @@
+def xo(s):
+    x=0
+    o=0
+    string = s.lower()
+    for letter in string:
+        if letter == 'x':
+            x+=1
+        elif letter == 'o':
+            o+=1
+        else:
+            pass
+    if x==o:
+        return True
+    else:
+        return False
