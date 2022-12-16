100db7b379f48fbdac8b6431d7468049f0e78462
{ ow3n } <35806309+wtfow3n@users.noreply.github.com>
2022-12-15 21:39:52 -0500

    Create TenMinuteWalk.py

---
+++ TenMinuteWalk.py
@@ -0,0 +1,23 @@
+def is_valid_walk(walk):
+    if len(walk) != 10: # check if a valid walk
+        return False
+    else:
+        x = ''.join(walk) # turn list into string
+        n=0
+        e=0
+        s=0
+        w=0
+        for dir in x: # parse string char by char
+            if dir == 'n':
+                n+=1
+            elif dir == 'e':
+                e+=1
+            elif dir == 's':
+                s+=1
+            elif dir == 'w':
+                w+=1
+
+        if (n-s == 0 and e-w == 0): # check to see if directions cancel eachother out...
+            return True
+        else:
+            return False
